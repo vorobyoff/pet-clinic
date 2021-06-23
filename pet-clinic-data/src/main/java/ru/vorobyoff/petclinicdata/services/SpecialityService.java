99d4e8d02541fc39d0cf079bc62ea148fd42d0b9
vorobyov <vorobyoffa@gmail.com>
2021-06-23 11:51:43 +0500

    Added SpecialityService.java and SpecialityServiceMapImpl.java.

---
+++ pet-clinic-data/src/main/java/ru/vorobyoff/petclinicdata/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package ru.vorobyoff.petclinicdata.services;
+
+import ru.vorobyoff.petclinicdata.models.Speciality;
+
+public interface SpecialityService extends CommonCrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/ru/vorobyoff/petclinicdata/services/map/SpecialityServiceMapImpl.java
@@ -0,0 +1,37 @@
+package ru.vorobyoff.petclinicdata.services.map;
+
+import org.springframework.stereotype.Service;
+import ru.vorobyoff.petclinicdata.models.Speciality;
+import ru.vorobyoff.petclinicdata.services.SpecialityService;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public class SpecialityServiceMapImpl extends AbstractMapService<Speciality, Long> implements SpecialityService {
+
+    @Override
+    public Collection<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Optional<Speciality> findById(final Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public void delete(final Speciality obj) {
+        super.delete(obj);
+    }
+
+    @Override
+    public void deleteById(final Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Speciality save(final Speciality obj) {
+        return super.save(obj);
+    }
+}
