55c51d06209f78ebfebf45effe6c55ca052f5edb
vorobyov <vorobyoffa@gmail.com>
2021-07-01 14:20:12 +0500

    Added necessary annotation.

--- pet-clinic-web/src/main/java/ru/vorobyoff/petclinicweb/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/ru/vorobyoff/petclinicweb/PetClinicApplication.java
@@ -1,10 +1,14 @@
 package ru.vorobyoff.petclinicweb;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import static org.springframework.boot.SpringApplication.run;
 
-@SpringBootApplication(scanBasePackages = {"ru.vorobyoff.petclinicdata", "ru.vorobyoff.petclinicweb"})
+@SpringBootApplication(scanBasePackages = {"ru.vorobyoff.petclinicdata.services", "ru.vorobyoff.petclinicweb"})
+@EnableJpaRepositories("ru.vorobyoff.petclinicdata.repositories")
+@EntityScan("ru.vorobyoff.petclinicdata.models")
 public class PetClinicApplication {
 
     public static void main(String... args) {
