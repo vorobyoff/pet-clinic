6bac5cf0ef0817dbd8f10ccaa120a5ada51500e5
vorobyov <vorobyoffa@gmail.com>
2021-07-06 17:46:27 +0500

    BaseEntity.java refactored for Lombok.

--- pet-clinic-data/src/main/java/ru/vorobyoff/petclinicdata/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/ru/vorobyoff/petclinicdata/models/BaseEntity.java
@@ -1,33 +1,24 @@
 package ru.vorobyoff.petclinicdata.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor(onConstructor_ = @Deprecated, access = PROTECTED)
 public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
-
-    public BaseEntity(final Long id) {
-        this.id = id;
-    }
-
-    @Deprecated
-    // Using only for JPA
-    protected BaseEntity() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
 }
